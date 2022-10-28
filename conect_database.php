3dea0506defebf64475a01554ad45a008529419c
ProgrammedMasah <115900305+ProgrammedMasah@users.noreply.github.com>
2022-10-28 16:40:57 +0200

    Update conect_database.php

--- conect_database.php
+++ conect_database.php
@@ -1,4 +1,6 @@
 <?php
+// Create connection
 $conn = mysqli_connect("sql780.main-hosting.eu","u508226309_masah_store","PASSword?q=masah_store_1","u508226309_masah_store");
+// Check connection
 if(!$conn)
-    echo "Failed to connect to MySQL". mysqli_conect_error();
+    echo "Failed to connect to MySQL". mysqli_connect_error();
